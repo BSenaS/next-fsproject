c06744679f72b50bcc6960d34f0900550c21d558
Batuhan Sena Sert <batuhansenasert@gmail.com>
2024-05-12 17:39:22 +0300

    1.Routing for the project.

---
+++ src/app/blog/[slug]/page.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+//NextJS içerisin de, dinamik route için [slug] ya da [id] folderlarını kullan.
+const SinglePostPage = () => {
+  return <div>SinglePostPage</div>;
+};
+
+export default SinglePostPage;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,5 @@
 const Home = () => {
-  return <div>Hello World!</div>;
+  return <div>Burası HomePage olacak</div>;
 };
 
 export default Home;
